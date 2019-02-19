99df22e6630aac7b23044f1d4144b4c6adeb4e95
Mike Landers <landers.mike@gmail.com>
2019-02-19 10:47:38 -0800

    switched to async

--- index.js
+++ index.js
@@ -10,64 +10,54 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints heres
-server.post('/api/zoos', (req, res) => {
-	db.insert(req.body)
-		.into('zoos')
-		.then(res => {
-			res.status(201).json(res);
-		})
-		.catch(err => {
-			res.status(500).json(err);
-		});
+server.post('/api/zoos', async (req, res) => {
+	try {
+		const id = await db('zoos').insert(req.body);
+		res.status(201).json(id);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
-server.get('/api/zoos', (req, res) => {
-	db('zoos')
-		.then(zoos => {
-			res.status(200).json(zoos);
-		})
-		.catch(err => {
-			res.status(500).json(err);
-		});
+server.get('/api/zoos', async (req, res) => {
+	try {
+		const result = await db('zoos');
+		res.status(200).json(result);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
-server.get('/api/zoos/:id', (req, res) => {
-	const { id } = req.params;
-	db('zoos')
-		.where('id', id)
-		.then(zoo => {
-			res.status(200).json(zoo);
-		})
-		.catch(err => {
-			res.status(500).json(err);
-		});
+server.get('/api/zoos/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const result = await db('zoos');
+		res.status(200).json(result);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
-server.delete('/api/zoos/:id', (req, res) => {
-	const { id } = req.params;
-	db('zoos')
-		.where('id', id)
-		.del()
-		.then(zoo => {
-			res.status(200).json(zoo);
-		})
-		.catch(err => {
-			res.status(500).json(err);
-		});
+server.delete('/api/zoos/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const result = db('zoos').where('id', id);
+		res.status(200).json(result);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
-server.put('/api/zoos/:id', (req, res) => {
-	const { id } = req.params;
-
-	db('zoos')
-		.where('id', id)
-		.update(req.body)
-		.then(zoo => {
-			res.status(200).json(zoo);
-		})
-		.catch(err => {
-			res.status(500).json(err);
-		});
+server.put('/api/zoos/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const result = db('zoos')
+			.where('id', id)
+			.update(req.body);
+		res.status(200).json(result);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
 const port = 3300;
